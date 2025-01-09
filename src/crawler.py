79f208e6126846ebdd5f320d2c7f35766bb557d7
Hero <jacobtaylor048@gmail.com>
2025-01-08 22:10:01 -0500

    Finalized scraper

--- src/crawler.py
+++ src/crawler.py
@@ -4,8 +4,11 @@ import requests
 
 from urllib.parse import urlparse, urljoin, urlunparse
 
+import utils
 
-#go over if I should catch the ejection for a get request and continue the program not terminate it. Like in the get request
+
+# go over if I should catch the ejection for a get request and continue the program not terminate it. Like in the get request
+
 
 def getHtml(url: str):  # add in if the http is not included format the url
     PREFIX = "https://"  # update this so we only validate urls also update for http
@@ -29,51 +32,79 @@ def getHtml(url: str):  # add in if the http is not included format the url
 
     except requests.exceptions.RequestException as e:
         print(f"ERROR: {e}")
+        raise
 
 
-def crawHtmlForForms(html: str):
-    forms = BeautifulSoup(html, "html.parser").find_all("form")
+def crawHtmlForForms(linksSet: set):
+    listForms = []
+    for link in linksSet:
+        try:
+            listForms.append(
+                BeautifulSoup(getHtml(link), "html.parser").find_all("form")
+            )
 
-    if len(forms) == 0:
-        raise Exception("No forms found in the HTML. Cannot proceed.")
-
-    else:
-        return forms
+        except Exception:
+            continue
+    return listForms
 
 
 # recursively find all links to a website
-def crawlWebsite(rootUrl: str) -> list:
-    return crawlerHelper(set(), rootUrl)
+def crawlWebsite(url: str) -> list:
+    return crawlerHelper(set(), set(), url, urlparse(url).netloc)
 
 
-def crawlerHelper(setUrl: set, listOfLinks:list, url: str) -> list:
+def crawlerHelper(seen: set, setLinks: set, url: str, baseDomain: str) -> set:
     # base case
-    if url in set or len(set) == 500 or urlDepth(url) > 3:
-        return list
-    
-    setUrl.add(url)
-    responseHtml = getHtml(url)
-    
+    if url in seen or len(seen) == 500 or urlDepth(url) > 3:
+        return setLinks
+
+    seen.add(url)
+
+    try:
+        responseHtml = getHtml(url)
+    except Exception:
+        return setLinks
+
     hrefList = BeautifulSoup(responseHtml, "html.parser").find_all("a")
-    
-    if len(hrefList) ==0:
-        return list
-    
+
+    if len(hrefList) == 0:
+        return setLinks
+
     else:
         for href in hrefList:
-           data =  href.get("href")
-           listOfLinks.append(data)
-    
-    return crawlerHelper(setUrl,listOfLinks,listOfLinks.pop()) #need to figure out this logic
+            data = href.get("href")
+            updateLink = urlPath(data, baseDomain).lower().rstrip("/")
+            if isValidUrl(updateLink, baseDomain) and updateLink not in seen:
+                setLinks.add(updateLink)
 
-    #maybe return a list of urls that are closure like [crawlerHelper(url),crawlerHelper(url),crawlerHelper(url)]
-    
-        
+        for link in setLinks:
+            return crawlerHelper(seen, setLinks, link, baseDomain)
+
+
 # -1 if not a valid url
 def urlDepth(url: str) -> int:
     urlPath = urlparse(url).path
 
     return len(list(filter(None, urlPath.split("/"))))
+
+
+def isValidUrl(url: str, baseDomain):
+    return (
+        not url.lower().endswith(tuple(utils.FILETYPES))
+        and urlparse(url).netloc == baseDomain
+    )
+
+
+def urlPath(url: str, baseDomain):
+    if (
+        urlparse(url).scheme == ""
+        and urlparse(url).netloc == ""
+        and urlparse(url).path != ""
+    ):
+        return normalizeUrl(baseDomain, url)  # relative to absolute
+
+    else:
+        return url  # absoluteurl
 
 
 def normalizeUrl(base_url: str, relative_url: str) -> str:
@@ -108,6 +139,6 @@ def normalizeUrl(base_url: str, relative_url: str) -> str:
 
     return normalized_url
 
-list1 = list()
-crawlerHelper(set(),list1,"https://fantasyislandsalem.com")
-print(list1)
+
+print(crawHtmlForForms(crawlWebsite("https://spillaneandson.com")))
+
---
+++ src/utils.py
@@ -0,0 +1,18 @@
+FILETYPES = [
+    ".pdf",
+    ".doc",
+    ".docx",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx",
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".mp3",
+    ".mp4",
+    ".zip",
+    ".rar",
+    ".exe",
+]
